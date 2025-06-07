83b99e252da3895fc4c596e36b94b436a1d7c1d6
NelsonQ2025 <nequispe11@umsa.bo>
2025-06-07 00:35:23 -0400

    Add files via upload

---
+++ PROYECTOWEB3-main/adopcion/pagina/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('registro/', views.registro_view, name='registro'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('<int:mascota_id>',views.Historial_Mascota,name='mascota'),
+]
