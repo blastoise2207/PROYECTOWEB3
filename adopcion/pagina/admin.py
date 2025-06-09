28db85c1e0cdb255b3f909e9136a433d3d89cf16
Brian AB <brianangulo900@gmail.com>
2025-06-09 18:50:00 -0400

    Registro de Modelos en admin.py

--- adopcion/pagina/admin.py
+++ adopcion/pagina/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Perfil1
 # Register your models here.
+admin.site.register(Perfil1)
--- adopcion/solicitud/templates/solicitud/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Solicitud,Mascota
-# Register your models here.
-admin.site.register(Solicitud)
-admin.site.register(Mascota)
